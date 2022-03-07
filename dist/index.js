6d0a1dac5fef3292535d2c1a6b6a937f348b9f1b
jonatan-arn <jonatanarnandis@hotmail.es>
2022-03-07 13:05:01 +0100

    heroku test 3

--- dist/index.js
+++ dist/index.js
@@ -4,20 +4,19 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const socketIo = require("socket.io");
-const router = express.Router();
 app.set("port", process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, "../", "public")));
 const server = app.listen(app.get("port"), () => {
     console.log("server on port", app.get("port"));
-});
-router.get("/", function (_req, res) {
-    res.send("Truc api");
 });
 const io = socketIo(server, {
     cosr: {
         origin: "*",
         methods: ["GET", "POST"],
     },
+});
+app.get("/", (_req, res) => {
+    res.send("Truc api");
 });
 require("./server/rooms.js")(io);
 require("./server/game.js")(io);
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const socketIo = require("socket.io");
-const router = express.Router();
 
 //Setting
 app.set("port", process.env.PORT || 3000);
@@ -16,15 +15,16 @@ app.use(express.static(path.join(__dirname, "../", "public")));
 const server = app.listen(app.get("port"), () => {
   console.log("server on port", app.get("port"));
 });
-router.get("/", function (_req: any, res: any) {
-  res.send("Truc api");
-});
+
 //Web socket
 const io: Server = socketIo(server, {
   cosr: {
     origin: "*",
     methods: ["GET", "POST"],
   },
+});
+app.get("/", (_req: any, res: any) => {
+  res.send("Truc api");
 });
 require("./server/rooms.js")(io);
 require("./server/game.js")(io);
